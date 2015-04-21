01f1a6540f8cdaf59360f9335ec8076a5b98dd64
Michael Bestas <mikeioannina@gmail.com>
2015-04-22 02:15:28 +0300

    SystemUI: Add heads up tile and fix on ambient display
    This also includes:
    
    commit d89a0c137a1d0969b506783d6db0e7753aea7ceb
    Author: ezio84 <brabus84@gmail.com>
    Date:   Wed Feb 20 20:14:27 2019 +0530
    
        Unbreak ambient display while Heads up is disabled
    
        Google tied in heads up with ambient display and so if you disabled heads up,
        you would only see a blank screen with LED pulsing. This commit fixes that!
    
    Change-Id: I33fe60c5f6ef9c5e0ab70f81291f3c71f1845666

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -36,6 +36,7 @@ import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.GamingModeTile;
+import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
@@ -125,6 +126,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new SoundTile(mHost);
             case "cpuinfo":
                 return new CPUInfoTile(mHost);
+            case "heads_up":
+                return new HeadsUpTile(mHost);
         }
 
         // Intent tiles.
