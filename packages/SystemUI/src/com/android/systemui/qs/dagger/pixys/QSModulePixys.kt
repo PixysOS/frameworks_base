b1b7cffd7a0511349ac59b899e1608bc63f2e309
Adnan Begovic <adnan@cyngn.com>
2015-12-11 16:16:02 -0800

    SystemUI: Add AmbientDisplay tile
    Co-authored-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: Iea339685f523016876f059052e66e97fd5170a81

--- packages/SystemUI/src/com/android/systemui/qs/dagger/pixys/QSModulePixys.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/pixys/QSModulePixys.kt
@@ -17,6 +17,7 @@
 package com.android.systemui.qs.dagger.pixys
 
 import com.android.systemui.qs.tileimpl.QSTileImpl
+import com.android.systemui.qs.tiles.AmbientDisplayTile
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.PowerShareTile
@@ -32,6 +33,12 @@ import dagger.multibindings.StringKey
 @Module
 interface QSModulePixys {
 
+    /** Inject AmbientDisplayTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(AmbientDisplayTile.TILE_SPEC)
+    fun bindAmbientDisplayTile(ambientDisplayTile: AmbientDisplayTile): QSTileImpl<*>
+    
     /** Inject CaffeineTile into tileMap in QSModule */
     @Binds
     @IntoMap
