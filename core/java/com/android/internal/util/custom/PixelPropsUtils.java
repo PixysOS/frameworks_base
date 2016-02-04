76778811b83a645a12536ac11bb5158526aca604
Keith Mok <kmok@cyngn.com>
2016-02-04 11:10:22 -0800

    SystemUI: Add caffeine QS tile
    Author: Keith Mok <kmok@cyngn.com>
    Date:   Thu Feb 4 11:10:22 2016 -0800
    
        SystemUI: Add caffeine qs tile
    
        Change-Id: I5d4aa97ae63fc0bcc40f0fa7d9305efbba091029
    
    Author: chillger <alexander.loskutov@gmail.com>
    Date:   Fri May 22 13:16:00 2020 +0200
    
        SystemUI: Long press Caffeine for infinite duration
    
        Change-Id: Ic49539dcda6d7d6b1dab362eb8ec168d2ec65b07
    
    * SKULSHADY: Use MD2 icon for Caffeine tile.
    
    Change-Id: I0b9a498285930cc397b7c4d20ddec3f447e6d332
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -40,7 +40,9 @@ public class PixelPropsUtils {
             "com.google.android.apps.turbo",
             "com.google.android.as",
             "com.google.android.apps.wallpaper",
-            "com.google.android.apps.maps"
+            "com.google.android.apps.maps",
+            "com.google.android.googlequicksearchbox",
+            "com.google.android.apps.fitness"
     };
 
     static {
@@ -50,14 +52,14 @@ public class PixelPropsUtils {
         propsToChange.put("DEVICE", "crosshatch");
         propsToChange.put("PRODUCT", "crosshatch");
         propsToChange.put("MODEL", "Pixel 3 XL");
-        propsToChange.put("FINGERPRINT", "google/crosshatch/crosshatch:11/RQ1A.210205.004/7038034:user/release-keys");
+        propsToChange.put("FINGERPRINT", "google/crosshatch/crosshatch:11/RQ2A.210305.006/7119741:user/release-keys");
         propsToChangePixel5 = new HashMap<>();
         propsToChangePixel5.put("BRAND", "google");
         propsToChangePixel5.put("MANUFACTURER", "Google");
         propsToChangePixel5.put("DEVICE", "redfin");
         propsToChangePixel5.put("PRODUCT", "redfin");
         propsToChangePixel5.put("MODEL", "Pixel 5");
-        propsToChangePixel5.put("FINGERPRINT", "google/redfin/redfin:11/RQ1C.210205.006/7047959:user/release-keys");
+        propsToChangePixel5.put("FINGERPRINT", "google/redfin/redfin:11/RQ2A.210305.007/7124944:user/release-keys");
     }
 
     public static void setProps(String packageName) {
