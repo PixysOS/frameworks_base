393a42dd11354c27fd3f75c87facc7fc152502c1
Alex <mazdarider23@gmail.com>
2016-09-07 03:45:27 -0400

    core: bring back PixysUtils
    In M we had too many of these. Let's establish this one early and just add checks
    to it as needed.
    
    - Add check for flashlight API
    
    Change-Id: I4a6b8ce1c365ed88b3fb14d06f4551c3d37519ec
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Utils: Add isWiFi only Method
    
    Change-Id: Id68bbb59811a33b57903a4a9b75c215d86252e80
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Utils: Fix isWiFi for API deprecations
    
    Change-Id: Ida8fade59dc6c10a17f93ed5384a8d30c9c4c918
    
    Utils: Add isChinese Language
    
    Change-Id: Ia9ecf8d05dcd0bfe53cdfb7a7b02651cccc9cc28
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Utils: Add isAvailableApp method
    
    Change-Id: Iefdaf3c0b2d5db688f3a3f2f56fbf08b535d37e4
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Utils: Add isPackageInstalled boolean
    
    Change-Id: Ibadea83b68c012c08e47c3622ba96a37c8ddf1b9
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Utils: Add isAppInstalled
    
    Change-Id: I15189741359537e0790a97e3ae832f503388ef7f
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Add api to toggle flashlight and check if device has flashlight
    
    statusbar service getter borrowed from PhoneWindowManager code
    
    use PackageManager to check if the device has flashlight
    
    use aosp flashcontroller to handle the flash toggle like the flashlight
    tile does
    
    Change-Id: I4e8fe3351b408aa55573b191baa1bc742962a85c
    
    Flashlight toggle api improvements
    
    being sure mCameraId is initialized.
    
    @Stallix - Adapt for A13.
    
    Change-Id: Ide72c7143c0e04715ca5404cd7f8e02333ac455b
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    
    Utils: Method to detect a notch'd device
    
    Change-Id: I4742eaeea94c4495361bdfe2b855ae0de0aa24c1
    
    Add a sendKeycode api to SparkUtils
    
    code borrowed from com.android.systemui.statusbar.policy.KeyButtonView
    
    Change-Id: I776f4904f663a34d2a094146d69dc5f75aa7a83a
    
    SparkUtils: Add isVoiceCapableMethod for in-call vibrations
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    
    Utils: Add util for Blendcolorforpercent
    
    Change-Id: Icc9b8a78535ab8f77fa2e23b44c501b95530427f
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesCommandQueueCallbacks.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesCommandQueueCallbacks.java
@@ -82,6 +82,7 @@ public class CentralSurfacesCommandQueueCallbacks implements CommandQueue.Callba
     private final com.android.systemui.shade.ShadeController mShadeController;
     private final CommandQueue mCommandQueue;
     private final ShadeViewController mShadeViewController;
+    private final FlashlightController mFlashlightController;
     private final RemoteInputQuickSettingsDisabler mRemoteInputQuickSettingsDisabler;
     private final MetricsLogger mMetricsLogger;
     private final KeyguardUpdateMonitor mKeyguardUpdateMonitor;
@@ -555,4 +556,5 @@ public class CentralSurfacesCommandQueueCallbacks implements CommandQueue.Callba
             mFlashlightController.setFlashlight(!mFlashlightController.isEnabled());
         }
     }
+
 }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -223,6 +223,7 @@ import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController.DeviceProvisionedListener;
 import com.android.systemui.statusbar.policy.ExtensionController;
 import com.android.systemui.statusbar.policy.HeadsUpManager;
+import com.android.systemui.statusbar.policy.FlashlightController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.policy.UserInfoControllerImpl;
 import com.android.systemui.statusbar.window.StatusBarWindowController;
@@ -549,6 +550,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
     private int mLastLoggedStateFingerprint;
     private boolean mIsLaunchingActivityOverLockscreen;
 
+    private FlashlightController mFlashlightController;
     private final LifecycleRegistry mLifecycle = new LifecycleRegistry(this);
     protected final BatteryController mBatteryController;
     private UiModeManager mUiModeManager;
@@ -654,6 +656,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
             NavigationBarController navigationBarController,
             AccessibilityFloatingMenuController accessibilityFloatingMenuController,
             Lazy<AssistManager> assistManagerLazy,
+            FlashlightController flashlightController,
             ConfigurationController configurationController,
             NotificationShadeWindowController notificationShadeWindowController,
             Lazy<NotificationShadeWindowViewController> notificationShadeWindowViewControllerLazy,
@@ -759,6 +762,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
         mNavigationBarController = navigationBarController;
         mAccessibilityFloatingMenuController = accessibilityFloatingMenuController;
         mAssistManagerLazy = assistManagerLazy;
+        mFlashlightController = flashlightController;
         mConfigurationController = configurationController;
         mNotificationShadeWindowController = notificationShadeWindowController;
         mNotificationShadeWindowViewControllerLazy = notificationShadeWindowViewControllerLazy;
@@ -1804,6 +1808,10 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
 
         if (mLightBarController != null) {
             mLightBarController.dump(pw, args);
+        }
+
+        if (mFlashlightController != null) {
+            mFlashlightController.dump(pw, args);
         }
 
         pw.println("SharedPreferences:");
