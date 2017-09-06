dfca630596a6612a426ff9cd94776083d319e5a2
Keith Mok <kmok@cyngn.com>
2017-09-06 09:03:56 +0800

    SystemUI: add caffeine qs tile
    Aex edits:
    Bring to S
    
    [aswin7469] Adapted for 13 QPR3
    
    Change-Id: I7299a8f25f2a65ca0e5a8af0ee7d28a580a6359e
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
@@ -55,6 +55,7 @@ import com.android.systemui.model.SysUiState;
 import com.android.systemui.motiontool.MotionToolModule;
 import com.android.systemui.navigationbar.NavigationBarComponent;
 import com.android.systemui.notetask.NoteTaskModule;
+import com.android.systemui.pixys.CaffeineModule;
 import com.android.systemui.people.PeopleModule;
 import com.android.systemui.plugins.BcSmartspaceConfigPlugin;
 import com.android.systemui.plugins.BcSmartspaceDataPlugin;
@@ -141,6 +142,7 @@ import dagger.Provides;
             AssistModule.class,
             BiometricsModule.class,
             BouncerViewModule.class,
+            CaffeineModule.class,
             ClipboardOverlayModule.class,
             ClockInfoModule.class,
             ClockRegistryModule.class,
