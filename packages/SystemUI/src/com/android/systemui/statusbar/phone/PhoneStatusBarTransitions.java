eed8ac520270addad5ff320dd69a0d1ae1eca48a
Sam Mortimer <sam@mortimer.me.uk>
2018-01-13 23:26:48 -0800

    SystemUI: Network Traffic [1/2]
    *) Add lineage-sdk internal NetworkTraffic to the statusbar
       using the LineageStatusBarItemHolder helper.
    
    Ported from lineagesdk:
    
    commit 7ea84eb2f97fb1bf162cbd2d346f0f6e55561dea
    Author: Jon Haus <haus.xda@gmail.com>
    Date:   Sat Dec 6 17:19:23 2014 +0100
    
        lineage-sdk: Add Network Traffic [2/3]
    
        Changes made since the original implementation from older branches:
    
        *) Forward-ported to Oreo and adapted to Lineage SDK (bgcngm).
    
        *) Implement LineageStatusBarItem interfaces to receive visibility
           and color tint information (bgcngm / sam3000).
    
        *) Move from fw/b to lineage-sdk (sam3000).
    
        *) Increase refresh interval from 1s to 2s (sam3000).
    
        *) Don't generate messages when the statusbar isn't visible (sam3000).
    
        *) Allow for choice of unit to be kb/s or Mb/s (and kB/s and MB/s). This
           deprecates threshold selection for autohide (sam3000).
    
        *) Add an option for whether units should be shown in the
           statusbar (sam3000).
    
        *) Various other simplifications (sam3000).
    
        *) Added vector drawables (courtesy of kover).
    
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Change-Id: I88d89955a94f21a4a105c33debe53bb68a58706b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -32,7 +32,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final PhoneStatusBarView mView;
     private final float mIconAlphaWhenOpaque;
 
-    private View mLeftSide, mStatusIcons, mBattery, mCenterClock;
+    private View mLeftSide, mStatusIcons, mBattery, mCenterClock, mNetworkTraffic;
 
     private Animator mCurrentAnimation;
 
@@ -47,6 +47,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mLeftSide = mView.findViewById(R.id.status_bar_left_side);
         mStatusIcons = mView.findViewById(R.id.statusIcons);
         mBattery = mView.findViewById(R.id.battery);
+        mNetworkTraffic = mView.findViewById(R.id.network_traffic);
         mCenterClock = mView.findViewById(R.id.center_clock);
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
@@ -91,6 +92,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
                     animateTransitionTo(mLeftSide, newAlpha),
                     animateTransitionTo(mStatusIcons, newAlpha),
                     animateTransitionTo(mBattery, newAlphaBC),
+                    animateTransitionTo(mNetworkTraffic, newAlpha)
                     animateTransitionTo(mCenterClock, newAlphaBC)
                     );
             if (isLightsOut(mode)) {
@@ -101,6 +103,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         } else {
             mLeftSide.setAlpha(newAlpha);
             mStatusIcons.setAlpha(newAlpha);
+            mNetworkTraffic.setAlpha(newAlpha);
             mBattery.setAlpha(newAlphaBC);
             mCenterClock.setAlpha(newAlphaBC);
         }
