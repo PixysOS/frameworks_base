8bbfa3f37403f0c52d5585da14261ab9a0e30339
ezio84 <brabus84@gmail.com>
2018-02-01 16:49:32 +0100

    Add api to force show led lights
    We can use this to show a led color preview when using the
    color picker, or any other stuff.
    
    use -1 as color int to switch off
    
    Change-Id: Id59f480e74a30a875c6640fdfc2e746d98a8513c

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -4216,6 +4216,16 @@ public class NotificationManagerService extends SystemService {
                     .exec(this, in, out, err, args, callback, resultReceiver);
         }
 
+        @Override
+        public void forceShowLedLight(int color) {
+            forceShowLed(color);
+        }
+
+        @Override
+        public void forcePulseLedLight(int color, int onTime, int offTime) {
+            forcePulseLed(color, onTime, offTime);
+        }
+
         /**
          * Get stats committed after startNs
          *
@@ -4312,6 +4322,22 @@ public class NotificationManagerService extends SystemService {
                 adjustments.remove(removeKey);
             }
             r.addAdjustment(adjustment);
+        }
+    }
+
+    private void forceShowLed(int color) {
+        if (color != -1) {
+            mNotificationLight.setColor(color);
+        } else {
+            mNotificationLight.turnOff();
+        }
+    }
+
+    private void forcePulseLed(int color, int onTime, int offTime) {
+        if (color != -1) {
+            mNotificationLight.setFlashing(color, Light.LIGHT_FLASH_TIMED, onTime, offTime);
+        } else {
+            mNotificationLight.turnOff();
         }
     }
 
