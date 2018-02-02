2f06591201396d76878df0d4dc720b5e86cda0ad
maxwen <max.weninger@gmail.com>
2018-02-02 14:55:57 +0100

    base: store custom fingerprint for package data
    and use that to clear package cache instead of our
    recovery script solution
    
    Change-Id: I21f599a84f8ff28b53bc088f739374c037f81be0

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -954,6 +954,12 @@ public class Build {
     public static final String FINGERPRINT = deriveFingerprint();
 
     /**
+     * @hide
+     */
+    public static final String PIXYS_FINGERPRINT = SystemProperties.get("ro.pixys.fingerprint",
+            deriveFingerprint());
+
+    /**
      * Some devices split the fingerprint components between multiple
      * partitions, so we might derive the fingerprint at runtime.
      */
