13def3c96418e42851d0e35df0e77895b2ffa458
Marko Man <darkobas@gmail.com>
2018-03-06 20:13:49 -0500

    base: Add support for theming notifications
    maxwen Fix notification text when using dark themes
    
    edit: don't add the config because subs exposures did it before
    
    Change-Id: Iea927eb3d95a9e9d07f07f61b2b420c95056dd5c
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/com/android/internal/util/NotificationColorUtil.java
+++ core/java/com/android/internal/util/NotificationColorUtil.java
@@ -489,6 +489,7 @@ public class NotificationColorUtil {
         final int resolvedColor = resolveColor(context, notificationColor);
 
         int color = resolvedColor;
+        isDark = isDark || context.getResources().getBoolean(R.bool.config_useDarkBgNotificationIconTextTinting);
         color = NotificationColorUtil.ensureTextContrast(color, backgroundColor, isDark);
 
         if (color != resolvedColor) {
