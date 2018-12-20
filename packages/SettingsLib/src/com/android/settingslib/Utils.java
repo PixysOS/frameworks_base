d19b280aecb1700e71052e0ab2777d4cb41ebf6a
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-12-20 10:27:47 -0200

    Remove now playing for now [3/4]
    Revert "Ambient Play: API rework"
    
    This reverts commit e8911cd621d6852ae5a08d9636586c2e47dcd60a.
    
    Revert "AmbientPlay: Introduce history manager [3/4]"
    
    This reverts commit 416ff274c8066734fa26839d4e6df66d555fc0a4.
    
    Revert "AmbientPlay: Move strings to fw/res"
    
    This reverts commit 236c073361fef4bfce130003329731227adcf9a8.
    
    Revert "AmbientPlay: Rework"
    
    This reverts commit 4319c0d0f4384f5c4e30f542cf0d5af34834e1fc.
    
    Revert "Now playing : Clean up old code and schedule now playing feature sensitive to Charging and Network Connection Type."
    
    This reverts commit 646305c8cc5d73d8c8d1da7a5e7e73d716bc4ea5.
    
    Revert "Now Playing : Multiple Changes"
    
    This reverts commit 8209643a564ffedfa5292b9c1ac1d0cc2e230ad3.
    
    Revert "RecoginitionObserverFactory : Simplify entire logic"
    
    This reverts commit 3c93c3da31241d48db97e938ce72f8226d37089c.
    
    Revert "Now Playing : Change recording duration to 10s and remove un-used return statement"
    
    This reverts commit ec007cd5264d003696401f5830291cce229ebb40.
    
    Revert "Now Playing : Various Improvements"
    
    This reverts commit 523beee611df1adf76bed4e3794dbbc47bcd305b.
    
    Revert "RecoginitionObserverFactory: Change Thread priority"
    
    This reverts commit 4a98befcc93eb6254b54d931f089b87f44baca84.
    
    Revert "RecoginitionObserverFactory : Check if we have aquired resources before releasing it."
    
    This reverts commit 793bec0c1d6bd3bf411329201eb93da8f9543868.
    
    Revert "RecoginitionObserverFactory: Why worry about charging status"
    
    This reverts commit 2d582ee705938fb90b9d127b069b7dbcc16b0e32.
    
    Revert "Ambient Play: Various fixes."
    
    This reverts commit a29be11da2bc9b10e0b1805ca8ee90824d6c5278.
    
    Revert "Ambient Play: Allow to configure the scan interval [1/2]."
    
    This reverts commit 2af2b47aea208185cacbe8df3efd788b149c6769.
    
    Revert "Ambient Play: Make it also work while charging. Why not?"
    
    This reverts commit e09e586febf311ccb19f7446333abfe44ca53a0a.
    
    Revert "Ambient Play: Hide AmbientIndication if Song and Artist are null."
    
    This reverts commit cbbd9fb77867347b7a74ce5bccffab30d3548caf.
    
    Revert "Ambient Play: Workaround for Pie (allow cleartext traffic)."
    
    This reverts commit 0125e56dbb5ebfa0b5a6f3c1e9fea2065fa96a11.
    
    Revert "Introduce Ambient Indication Interface"
    
    This reverts commit acd59e49fb53b6da407a8e9b6dc388e569218e92.
    
    Change-Id: Id772792330e18a2335bf0f66dd30aa147e7f7c77

--- packages/SettingsLib/src/com/android/settingslib/Utils.java
+++ packages/SettingsLib/src/com/android/settingslib/Utils.java
@@ -31,7 +31,6 @@ import com.android.settingslib.wrapper.LocationManagerWrapper;
 import java.text.NumberFormat;
 
 import com.android.internal.util.ambient.weather.WeatherClient;
-import com.android.internal.util.custom.ambient.play.AmbientPlayHistoryManager;
 
 public class Utils {
 
@@ -289,7 +288,6 @@ public class Utils {
                 || pkg.packageName.equals(sServicesSystemSharedLibPackageName)
                 || pkg.packageName.equals(sSharedSystemSharedLibPackageName)
                 || pkg.packageName.equals(PrintManager.PRINT_SPOOLER_PACKAGE_NAME)
-                || pkg.packageName.equals(AmbientPlayHistoryManager.SERVICE_PACKAGE)
                 || pkg.packageName.equals(WeatherClient.SERVICE_PACKAGE)
                 || isDeviceProvisioningPackage(resources, pkg.packageName);
     }
