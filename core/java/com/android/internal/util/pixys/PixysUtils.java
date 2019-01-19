404ba55e4f4bb48d67fe249f3a08bd2fb204ff3f
Alex Cruz <du.alexcruz@gmail.com>
2019-01-19 17:30:24 -0600

    Utils: Add method to determine battery temp
    Change-Id: Id0b8addc460afae030eac0a2bf064fb21aa2c4dc

--- core/java/com/android/internal/util/pixys/PixysUtils.java
+++ core/java/com/android/internal/util/pixys/PixysUtils.java
@@ -23,6 +23,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -310,5 +311,18 @@ public class PixysUtils {
                 }
             }
         }
+    }
+
+    public static String batteryTemperature(Context context, Boolean ForC) {
+        Intent intent = context.registerReceiver(null, new IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED));
+        float  temp = ((float) (intent != null ? intent.getIntExtra(
+                BatteryManager.EXTRA_TEMPERATURE, 0) : 0)) / 10;
+        // Round up to nearest number
+        int c = (int) ((temp) + 0.5f);
+        float n = temp + 0.5f;
+        // Use boolean to determine celsius or fahrenheit
+        return String.valueOf((n - c) % 2 == 0 ? (int) temp :
+                ForC ? c * 9/5 + 32 + "°F" :c + "°C");
     }
 }
