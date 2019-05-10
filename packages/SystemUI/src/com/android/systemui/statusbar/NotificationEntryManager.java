0fc74ac10afb7d0659505ac4bfdf6f99033706b8
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-10 12:03:25 -0300

    Whitelist default dialer app for heads up
    Change-Id: I445de456d72b88cd36e45139d27c0466aaf940b9

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationEntryManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationEntryManager.java
@@ -38,6 +38,7 @@ import android.service.notification.NotificationListenerService;
 import android.service.notification.NotificationStats;
 import android.service.notification.StatusBarNotification;
 import android.text.TextUtils;
+import android.telecom.TelecomManager;
 import android.util.ArraySet;
 import android.util.EventLog;
 import android.util.Log;
@@ -142,6 +143,7 @@ public class NotificationEntryManager implements Dumpable, NotificationInflater.
      */
     private final ArraySet<String> mKeysKeptForRemoteInput = new ArraySet<>();
 
+    private TelecomManager mTelecomManager;
 
     private final class NotificationClicker implements View.OnClickListener {
 
@@ -260,6 +262,7 @@ public class NotificationEntryManager implements Dumpable, NotificationInflater.
         mGroupManager.setPendingEntries(mPendingNotifications);
 
         mTrackInfoSeparator = mContext.getResources().getString(R.string.ambientmusic_songinfo);
+        mTelecomManager = (TelecomManager) mContext.getSystemService(Context.TELECOM_SERVICE);
     }
 
     public void setUpWithPresenter(NotificationPresenter presenter,
@@ -962,12 +965,14 @@ public class NotificationEntryManager implements Dumpable, NotificationInflater.
     }
 
     public boolean shouldPeek(NotificationData.Entry entry, StatusBarNotification sbn) {
-        if ((!mUseHeadsUp && !mPresenter.isDozing()) || mPresenter.isDeviceInVrMode()) {
+        String defaultDialerApp = mTelecomManager != null ? mTelecomManager.getDefaultDialerPackage() : "";
+        boolean isDialerApp = sbn.getPackageName().equals(defaultDialerApp);
+        if ((!mUseHeadsUp && !mPresenter.isDozing() && !isDialerApp) || mPresenter.isDeviceInVrMode()) {
             if (DEBUG) Log.d(TAG, "No peeking: no huns or vr mode");
             return false;
         }
 
-        if (mNotificationData.shouldFilterOut(entry)) {
+        if (mNotificationData.shouldFilterOut(entry) && !isDialerApp) {
             if (DEBUG) Log.d(TAG, "No peeking: filtered notification: " + sbn.getKey());
             return false;
         }
@@ -981,7 +986,7 @@ public class NotificationEntryManager implements Dumpable, NotificationInflater.
             return false;
         }
 
-        if (!mPresenter.isDozing() && mNotificationData.shouldSuppressPeek(entry)) {
+        if (!mPresenter.isDozing() && mNotificationData.shouldSuppressPeek(entry) && !isDialerApp) {
             if (DEBUG) Log.d(TAG, "No peeking: suppressed by DND: " + sbn.getKey());
             return false;
         }
