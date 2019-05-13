6e49af252fe74f784f98be6625d9e2c878acbd82
Ardjlon <carlosardany@gmail.com>
2019-05-12 21:51:03 -0600

    Fix 225693 SystemUI: add left and right virtual buttons while typing
    Change-Id: I9816a223b9b95b53654f2e847815e8062f156d02

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4881,6 +4881,17 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_ALBUM_ART_FILTER = "lockscreen_album_art_filter";
 
+       /**
+         * enable or disable single handed mode
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_MENU_ARROW_KEYS = "navigation_bar_menu_arrow_keys";
+
+        /** @hide */
+        private static final Validator NAVIGATION_BAR_MENU_ARROW_KEYS_VALIDATOR = BOOLEAN_VALIDATOR;
+
+
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
@@ -4955,7 +4966,8 @@ public final class Settings {
             STATUSBAR_CLOCK_DATE_STYLE,
             STATUSBAR_CLOCK_DATE_FORMAT,
             NOTIFICATION_LIGHT_PULSE,
-            DOZE_ON_CHARGE
+            DOZE_ON_CHARGE,
+	    NAVIGATION_BAR_MENU_ARROW_KEYS
         };
 
         /**
@@ -5023,6 +5035,7 @@ public final class Settings {
             PUBLIC_SETTINGS.add(HAPTIC_FEEDBACK_ENABLED);
             PUBLIC_SETTINGS.add(SHOW_WEB_SUGGESTIONS);
             PUBLIC_SETTINGS.add(VIBRATE_WHEN_RINGING);
+	    PUBLIC_SETTINGS.add(NAVIGATION_BAR_MENU_ARROW_KEYS);
         }
 
         /**
@@ -5091,6 +5104,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUSBAR_CLOCK_DATE_FORMAT);
             PRIVATE_SETTINGS.add(DEVICE_INTRODUCTION_COMPLETED);
             PRIVATE_SETTINGS.add(DOZE_ON_CHARGE);
+	    PRIVATE_SETTINGS.add(NAVIGATION_BAR_MENU_ARROW_KEYS);
         }
 
 
@@ -5210,6 +5224,7 @@ public final class Settings {
             VALIDATORS.put(DEVICE_INTRODUCTION_COMPLETED, DEVICE_INTRODUCTION_COMPLETED_VALIDATOR);
             VALIDATORS.put(NOTIFICATION_LIGHT_PULSE, BOOLEAN_VALIDATOR);
             VALIDATORS.put(DOZE_ON_CHARGE, DOZE_ON_CHARGE_VALIDATOR);
+	    VALIDATORS.put(NAVIGATION_BAR_MENU_ARROW_KEYS, NAVIGATION_BAR_MENU_ARROW_KEYS_VALIDATOR);
         }
 
         /**
