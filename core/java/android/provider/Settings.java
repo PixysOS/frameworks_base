dd8af9c5babaa0a09466fc3f3e95f356e58efdf1
Lucchetto <lucchetto.tie@live.com>
2019-07-21 17:26:08 +0530

    base: Smart Charging (1/4)
    Change-Id: I4833acd8d9270edf9ede89d09daedd04c86209da

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4992,6 +4992,16 @@ public final class Settings {
          */
         public static final String BATTERY_BAR_USE_GRADIENT_COLOR = "battery_bar_use_gradient_color";
 
+	/**
+         * @hide
+         */
+        public static final String SMART_CHARGING = "smart_charging";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CHARGING_LEVEL = "smart_charging_level";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
