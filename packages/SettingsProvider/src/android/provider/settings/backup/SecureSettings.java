f0359b83b90ff4d67ed6f93b2d4ef2434d96f459
Sam Mortimer <sam@mortimer.me.uk>
2019-08-15 19:40:55 -0700

    fw/b: Add capability to allow tethering to use VPN upstreams [1/3]
    Squash of:
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Thu Aug 15 19:40:55 2019 -0700
    
        fw/b: Add capability to allow tethering to use VPN upstreams
    
        * Toggled on/off at runtime via a new hotspot lineage setting.
    
        * Dynamically updates the tethering upstream for existing hotspot
          clients as VPNs are brought up / down or the hotspot setting
          is changed.
    
        * This implementation depends on fw/b config_tether_upstream_automatic
          being set to true.
    
        Change-Id: I2ac0b4acc0ea686dfdf54561cb3428808e337160
    
    Change-Id: I2363e9baa007eac25be04ce23f0de41b80c5da42
    
    ----
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Fri Sep 13 16:27:37 2019 -0700
    
        fw/b: Prevent trying to enable hw offload for tethering via VPN upstreams
    
        * Tethering via VPN upstream requires a sw path.
    
        * hw offload setup happened to be being disabled anyway owing to a fail
          return code from setDataLimit().  However, it was causing offload to be
          disabled entirely (until next hotspot off / on event).
    
        * Gracefully skip hw offload for vpn upstreams so that it is automatically
          used again when a vpn is disconnected.
    
        Change-Id: I4df13f02889305560903b7b1e919eedc7af78c07
    
    [mkbestas]: Remove LineageSettings dependency & port to android 11
    
    [uldiniad]: Replaced Settings.Secure private APIs and variables with alternatives
    
    [Android 12]: Tethering moved to packages/modules/Connectivity
    
    Change-Id: I77ed0142e653f4993486eea44e4dac21e3f67f17
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -263,5 +263,6 @@ public class SecureSettings {
         Settings.Secure.DOZE_DOUBLE_TAP_GESTURE_AMBIENT,
         Settings.Secure.DOZE_PICK_UP_GESTURE_AMBIENT,
         Settings.Secure.DOZE_SINGLE_TAP_GESTURE_AMBIENT,
+        Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS
     };
 }
