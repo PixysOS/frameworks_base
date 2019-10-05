45b0b4b80bcf98834a545b7ea9f035c88c47e77e
Mulugeta Engdaw <mengdaw@codeaurora.org>
2019-10-04 17:04:55 -0700

    Add mechanism to improve consistancy of notification
    Supports two states: add and remove
    added death recipient for hal handle
    
    CRs-Fixed: 2539384,2538995
    Change-Id: I50c94d8936d8cd07f7670eed4466aac65a6063c8

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1085,6 +1085,10 @@ public final class SystemServer {
             mSystemServiceManager.startService(PinnerService.class);
             traceEnd();
 
+            traceBeginAndSlog("ActivityTriggerService");
+            mSystemServiceManager.startService(ActivityTriggerService.class);
+            traceEnd();
+
             traceBeginAndSlog("SignedConfigService");
             SignedConfigService.registerUpdateReceiver(mSystemContext);
             traceEnd();
