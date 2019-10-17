e1d6567747566515a75d79948777c3fc5ab0aa72
Anushek Prasal <anushekprasal@gmail.com>
2019-10-17 12:54:50 +0530

    ScreenshotSound: Fix path for the file

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -605,7 +605,7 @@ class GlobalScreenshot {
 
         // Setup the Screenshot sound
         mScreenshotSound= RingtoneManager.getRingtone(mContext,
-                    Uri.parse("file://" + "/system/media/audio/ui/camera_click.ogg"));
+                    Uri.parse("file://" + "/system/product/media/audio/ui/camera_click.ogg"));
     }
 
     /**
