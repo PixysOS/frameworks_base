62f9f863998d2644ba4e643b6aa195b83138be9d
ezio84 <brabus84@gmail.com>
2019-11-03 21:02:11 +0100

    Ambient music: add Pixel Now Playing support
    show Now Playing info if no media player notification is active
    
    Change-Id: Ia358125084abb1e756656f3d4cbc9db5145c6c57
    Signed-off-by: Subinsmani <subins.mani@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -457,7 +457,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
     @Override
     public void onMetadataOrStateChanged(MediaMetadata metadata, @PlaybackState.State int state) {
         synchronized (this) {
-            boolean nextVisible = NotificationMediaManager.isPlayingState(state);
+            boolean nextVisible = NotificationMediaManager.isPlayingState(state) || mMediaManager.getNowPlayingTrack() != null;
             mMediaHandler.removeCallbacksAndMessages(null);
             if (mMediaIsVisible && !nextVisible && mStatusBarState != StatusBarState.SHADE) {
                 // We need to delay this event for a few millis when stopping to avoid jank in the
@@ -478,7 +478,8 @@ public class KeyguardSliceProvider extends SliceProvider implements
     }
 
     private void updateMediaStateLocked(MediaMetadata metadata, @PlaybackState.State int state) {
-        boolean nextVisible = NotificationMediaManager.isPlayingState(state);
+        boolean nowPlayingAvailable = mMediaManager.getNowPlayingTrack() != null;
+        boolean nextVisible = NotificationMediaManager.isPlayingState(state) || nowPlayingAvailable;
         CharSequence title = null;
         if (metadata != null) {
             title = metadata.getText(MediaMetadata.METADATA_KEY_TITLE);
@@ -496,6 +497,13 @@ public class KeyguardSliceProvider extends SliceProvider implements
         mMediaTitle = title;
         mMediaArtist = artist;
         mMediaIsVisible = nextVisible;
+
+        if (mMediaTitle == null && nowPlayingAvailable) {
+            mMediaTitle = mMediaManager.getNowPlayingTrack();
+            mMediaIsVisible = true;
+            mMediaArtist = null;
+        }
+
         notifyChange();
     }
 
