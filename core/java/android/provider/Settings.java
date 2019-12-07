723ece7aa293b73b21dbb9b30d7f6e1d3f6a90f3
AnkitGourav <Ankitgourav035@gmail.com>
2019-12-07 15:43:55 +0530

    Add Network Traffic Layout [1/2]
    - Added option to select horizontal/verticle layout
    - horizontal layout was same as before
    - verticle layout is based on https://github.com/ArrowOS/android_frameworks_base/commit/e211cafe844abac4fb417400fe2e3caa0ff41202
    - Update units to KiB & MiB from KB & MB as of IEC standards
    
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>
    
    Change-Id: I7ba19aaa7b3aeeed2ff833fb2fe6c86f9d83a82e
    
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I8a7c8aaee74a1498ccf4637fa46918304de92d61
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4831,6 +4831,12 @@ public final class Settings {
          */
          public static final String NETWORK_TRAFFIC_FONT_SIZE = "network_traffic_font_size";
 
+         /**
+         * Network traffic layout
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_LAYOUT = "network_traffic_layout";
+
         /**
          * Show network traffic indicator
          * 0 - Statusbar (default)
@@ -4964,6 +4970,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_FONT_SIZE);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_VIEW_LOCATION);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_LAYOUT);
         }
 
         /**
