1a43fb3b64b7f45fea1eb60a86cf8eec4b08839b
spezi77 <spezi7713@gmx.net>
2020-01-05 14:26:55 +0100

    fwb: Move icon shapes to PixysTheme [1/2].
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: Ia815b5fd40c2c32f8caaaede15621e9dcbcfc5cd

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -41,10 +41,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconPackRoundedLauncherOverlay \
 	IconPackRoundedSettingsOverlay \
 	IconPackRoundedSystemUIOverlay \
-	IconPackRoundedThemePickerUIOverlay \
-	IconShapeRoundedRectOverlay \
-	IconShapeSquircleOverlay \
-	IconShapeTeardropOverlay \
+	IconPackRoundedThemePickerOverlay \
 	NavigationBarMode3ButtonOverlay \
 	NavigationBarMode2ButtonOverlay \
 	NavigationBarModeGesturalOverlay \
