41de9ceb64affd1ada8589db56702a6faee37f97
limoniumstatice <strangeholic@gmail.com>
2020-01-23 03:33:39 +0000

    BatteryMeterView: Fix logcat warning spam
    * This spam occurs when displaying keyguard.
    
     2672  2672 W View    : requestLayout() improperly called by android.widget.TextView{8e47ff1 V.ED..... ......ID 0,0-78,105 #7f0a007c app:id/battery_percentage_view} during layout: running second layout pass
     2672  2672 W View    : requestLayout() improperly called by android.widget.TextView{8e47ff1 V.ED..... ......ID 0,0-78,105 #7f0a007c app:id/battery_percentage_view} during second layout pass: posting in next frame
    
    Change-Id: I461b1076103b57c23e4b05de9f2ce9705f3223a0
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -434,6 +434,7 @@ public class BatteryMeterView extends LinearLayout implements
                     mBatteryPercentView.setTextAppearance(mPercentageStyleId);
                 }
                 if (mTextColor != 0) mBatteryPercentView.setTextColor(mTextColor);
+                updatePercentText();
                 addView(mBatteryPercentView,
                         new ViewGroup.LayoutParams(
                                 LayoutParams.WRAP_CONTENT,
