dd30d85c0e84b952a713639082b7307b2a1bf358
ezio84 <brabus84@gmail.com>
2020-02-24 21:31:08 +0100

    Power flashlight: allow to use it when all ambient/wake options are off
    now that we linked the proximity sensor check with aosp ambient code,
    we need to enable doze services also when the user disables all ambient
    and wake options
    
    Signed-off-by: Akash Srivastava <akashniki@gmail.com>
    Change-Id: I33a1a78bdf94f73c7e79503c7fc3cf50b1db64c8
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -54,7 +54,8 @@ public class AmbientDisplayConfiguration {
                 || pickupGestureEnabled(user)
                 || tapGestureEnabled(user)
                 || doubleTapGestureEnabled(user)
-                || isAmbientTickerEnabled(user);
+                || isAmbientTickerEnabled(user)
+                || isPowerBtnFlashlightEnabled(user);
     }
 
     /** {@hide} */
@@ -225,5 +226,11 @@ public class AmbientDisplayConfiguration {
     public boolean isAmbientTickerEnabled(int user) {
         return Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.PULSE_ON_NEW_TRACKS, 1, user) != 0;
+    }
+
+    /** {@hide} */
+    public boolean isPowerBtnFlashlightEnabled(int user) {
+        return Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.TORCH_POWER_BUTTON_GESTURE, 0, user) != 0;
     }
 }
