83d803bf77f8bf3a422709b50368c3b2d2a11fb1
Subinsmani <subins.mani@gmail.com>
2020-03-05 08:12:59 +0530

    Revert "SystemUIFactory: Add scrimForBubble to LiveWallpaperScrimController"
    This reverts commit 59913f6c1806411d8484eacea6031aabde8f3333.
    
    Change-Id: I09a66c59924e53147886adffeda4b8232ab09ab8
    Signed-off-by: Subinsmani <subins.mani@gmail.com>

--- packages/SystemUI/src/com/google/android/systemui/SystemUIGoogleFactory.java
+++ packages/SystemUI/src/com/google/android/systemui/SystemUIGoogleFactory.java
@@ -18,12 +18,12 @@ import java.util.function.Consumer;
 
 public class SystemUIGoogleFactory extends SystemUIFactory {
     @Override
-    public ScrimController createScrimController(ScrimView scrimBehind, ScrimView scrimInFront, ScrimView scrimForBubble,
+    public ScrimController createScrimController(ScrimView scrimBehind, ScrimView scrimInFront,
             LockscreenWallpaper lockscreenWallpaper,
             TriConsumer<ScrimState, Float, GradientColors> scrimStateListener,
             Consumer<Integer> scrimVisibleListener, DozeParameters dozeParameters,
             AlarmManager alarmManager, KeyguardMonitor keyguardMonitor) {
-        return new LiveWallpaperScrimController(scrimBehind, scrimInFront, scrimForBubble, lockscreenWallpaper,
+        return new LiveWallpaperScrimController(scrimBehind, scrimInFront, lockscreenWallpaper,
                 scrimStateListener, scrimVisibleListener, dozeParameters, alarmManager, keyguardMonitor);
     }
 }
