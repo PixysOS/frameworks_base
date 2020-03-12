27ed3a90f489336650d076478ec1be65b77d367e
Park Ju Hyung <qkrwngud825@gmail.com>
2020-03-13 01:32:27 +0900

    [DO NOT MERGE] VibratorService: bypass AppOps check
    AppOps is randomly rejecting vibration calls from SystemUI.
    
    Bypass this check entirely to workaround it.
    
    This introduces a potential security issue where apps without
    android.permission.VIBRATE would be able to send vibrations,
    but the existing issue directly affects UX and outweighs the
    potential security issue.
    
    Change-Id: I5cc80eda2dbfe4a5baab95e961e98717ed5457f1
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- services/core/java/com/android/server/VibratorService.java
+++ services/core/java/com/android/server/VibratorService.java
@@ -1162,6 +1162,8 @@ public class VibratorService extends IVibratorService.Stub
             return false;
         }
 
+        // XXX: AppOps is randomly rejecting vibration calls from SystemUI
+        /*
         final int mode = getAppOpMode(vib.uid, vib.opPkg, vib.attrs);
         if (mode != AppOpsManager.MODE_ALLOWED) {
             if (mode == AppOpsManager.MODE_ERRORED) {
@@ -1171,6 +1173,7 @@ public class VibratorService extends IVibratorService.Stub
             }
             return false;
         }
+        */
 
         return true;
     }
