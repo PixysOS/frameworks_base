b1aabe87d4523c93f44056569adac5ba3ea4cb33
utzcoz <utzcoz@outlook.com>
2020-06-11 20:16:55 +0800

    Add back button for freeform window
    Thanks to RadekBledowski for the graphics
    
    Signed-off-by: utzcoz <utzcoz@outlook.com>
    Change-Id: Ic847315ef5fb606cacde03f30ad0ec41730d36b7

--- core/java/android/view/Window.java
+++ core/java/android/view/Window.java
@@ -676,6 +676,8 @@ public abstract class Window {
         void updateNavigationBarColor(int color);
 
         boolean moveTaskToBack(boolean nonRoot);
+
+        void onBackPressed();
     }
 
     /** @hide */
