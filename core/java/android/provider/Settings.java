0cb125ebc0d809164605a2947340c100cff173f2
Anay Wadhera <awadhera@berkeley.edu>
2020-07-21 00:28:29 -0700

    base: clipboard toast toggle [1/2]
    Change-Id: I1c8c47b1865eae864e44f4e0b682dee4fe07c827
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5456,6 +5456,13 @@ public final class Settings {
         public static final Validator REFRESH_RATE_SETTING_VALIDATOR = NON_NEGATIVE_INTEGER_VALIDATOR;
 
         /**
+         * Toggle for clipboard access toast.
+         * @hide
+         */
+
+        public static final String SHOW_CLIPBOARD_TOAST = "show_clipboard_toast";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
