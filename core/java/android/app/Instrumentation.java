7c90e2fb4a8e1094f6bd44062d44a3be41197518
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-19 20:46:32 -0300

    Squashed Import of PixelPropsUtils
    * Imported list from our 13 base [1] [2]
    * Removed pixel 6 pro spoof
    * Clean-ups and update logic with PropImitationHooks
    * Simplify and improve bypassing for PPU `onTaskStackChanged` listening
    * Allow to pass strong integrity by building rro
    
    [1] https://github.com/PixysOS/frameworks_base/commits/cb5e356ab56d1edea014a3f93ca36a9f2004a2bf/core/java/com/android/internal/util/pixys/PixelPropsUtils.java
    [2] https://github.com/PixysOS/frameworks_base/commits/cb5e356ab56d1edea014a3f93ca36a9f2004a2bf/core/java/com/android/internal/util/pixys/GamesPropsUtils.java
    [3] https://github.com/TheParasiteProject/frameworks_base/blob/432636254ebad0fd5e78c624fccfca9a40c6a0cf/core/java/com/android/internal/util/custom/PixelPropsUtils.java
    
    Co-authored-by: johnmart19 <johnivan19999@gmail.com>
    Co-authored-by: aswin7469 <aswinas@pixysos.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Dyneteve <dyneteve@hentaios.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Chris Crump <chriscrawford893@gmail.com>
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Change-Id: I859a9dfd7a4fc190cdd011607226eb5348a15551
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -62,6 +62,8 @@ import android.view.Window;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
+import com.android.internal.util.pixys.PixelPropsUtils;
+import com.android.internal.util.pixys.GamesPropsUtils;
 
 import java.io.File;
 import java.lang.annotation.Retention;
@@ -1351,6 +1353,8 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
+        PixelPropsUtils.setProps(app);
+        GamesPropsUtils.setProps(app);
         return app;
     }
     
@@ -1368,6 +1372,8 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
+        PixelPropsUtils.setProps(app);
+        GamesPropsUtils.setProps(app);
         return app;
     }
 
--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -288,6 +288,7 @@ import com.android.server.uri.NeededUriGrants;
 import com.android.server.uri.UriGrantsManagerInternal;
 import com.android.server.wallpaper.WallpaperManagerInternal;
 import com.android.wm.shell.Flags;
+import com.android.internal.util.pixys.PixelPropsUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -1981,7 +1982,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        enforceTaskPermission("getFocusedRootTaskInfo()");
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("getFocusedRootTaskInfo()");
+        }
         final long ident = Binder.clearCallingIdentity();
         try {
             synchronized (mGlobalLock) {
@@ -3105,7 +3108,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        enforceTaskPermission("registerTaskStackListener()");
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("registerTaskStackListener()");
+        }
         mTaskChangeNotificationController.registerTaskStackListener(listener);
     }
 
