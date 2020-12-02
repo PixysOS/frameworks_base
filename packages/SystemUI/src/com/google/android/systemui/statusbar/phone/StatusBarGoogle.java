d852b356dccc0aca1ba7388a99b659233b662167
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-12-02 00:28:32 +0000

    SystemUI: Handle cases that we can't address within FODCircleView
    So, briefly, what went on in this pow-wow is:
        - created a callback to efficiently monitor calls to FODCircleView impl (LuK1337 had a lot of patience)
        - addressed FOD visibility on statusbar expansion/collapse checking top activity package name and class within StatusBar.java
        - addressed FOD visibility if an app gets launched after statusbar expansion from a notif
        - addressed FOD visibility over every launcher animation, where every stands for:
            back to home, open recents, back gesture, assistant gesture (tested against any navigation type available)
        - reduced animation time to 125ms
    
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Change-Id: I4977540c72da557582e01d78da81980f76a8d9d7

--- packages/SystemUI/src/com/google/android/systemui/statusbar/phone/StatusBarGoogle.java
+++ packages/SystemUI/src/com/google/android/systemui/statusbar/phone/StatusBarGoogle.java
@@ -19,6 +19,7 @@ import com.android.systemui.dagger.qualifiers.UiBackground;
 import com.android.systemui.Dependency;
 import com.android.systemui.InitController;
 import com.android.systemui.assist.AssistManager;
+import com.android.systemui.biometrics.FODCircleViewImpl;
 import com.android.systemui.broadcast.BroadcastDispatcher;
 import com.android.systemui.bubbles.BubbleController;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
@@ -192,7 +193,8 @@ public class StatusBarGoogle extends StatusBar {
             KeyguardIndicationController keyguardIndicationController,
             DismissCallbackRegistry dismissCallbackRegistry,
             Lazy<NotificationShadeDepthController> notificationShadeDepthControllerLazy,
-            StatusBarTouchableRegionManager statusBarTouchableRegionManager) {
+            StatusBarTouchableRegionManager statusBarTouchableRegionManager,
+            FODCircleViewImpl fodCircleViewImpl) {
         super(context,
                 notificationsController,
                 lightBarController,
@@ -269,7 +271,8 @@ public class StatusBarGoogle extends StatusBar {
                 keyguardIndicationController,
                 dismissCallbackRegistry,
                 notificationShadeDepthControllerLazy,
-                statusBarTouchableRegionManager);
+                statusBarTouchableRegionManager,
+                fodCircleViewImpl);
         mSmartSpaceController = smartSpaceController;
         mWallpaperNotifier = wallpaperNotifier;
     }
--- packages/SystemUI/src/com/google/android/systemui/statusbar/phone/dagger/StatusBarGoogleModule.java
+++ packages/SystemUI/src/com/google/android/systemui/statusbar/phone/dagger/StatusBarGoogleModule.java
@@ -30,6 +30,7 @@ import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.ViewMediatorCallback;
 import com.android.systemui.InitController;
 import com.android.systemui.assist.AssistManager;
+import com.android.systemui.biometrics.FODCircleViewImpl;
 import com.android.systemui.broadcast.BroadcastDispatcher;
 import com.android.systemui.bubbles.BubbleController;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
@@ -201,7 +202,8 @@ public interface StatusBarGoogleModule {
             KeyguardIndicationController keyguardIndicationController,
             Lazy<NotificationShadeDepthController> notificationShadeDepthController,
             DismissCallbackRegistry dismissCallbackRegistry,
-            StatusBarTouchableRegionManager statusBarTouchableRegionManager) {
+            StatusBarTouchableRegionManager statusBarTouchableRegionManager,
+            FODCircleViewImpl fodCircleViewImpl) {
         return new StatusBarGoogle(
                 smartSpaceController,
                 wallpaperNotifier,
@@ -281,6 +283,7 @@ public interface StatusBarGoogleModule {
                 keyguardIndicationController,
                 dismissCallbackRegistry,
                 notificationShadeDepthController,
-                statusBarTouchableRegionManager);
+                statusBarTouchableRegionManager,
+                fodCircleViewImpl);
     }
 }
