d6ae64bb681723f3a6de643b3ac6b5e39d014b5f
Nick Reuter <nreuter85@gmail.com>
2021-05-30 02:30:01 -0500

    FODCircleView: fixup AOD fingerprint support
    Change-Id: Ia0538aae627e6314426790703cdf6183ebba71a1

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -354,7 +354,7 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         updateIconDim(false);
         if (key.equals(FOD_GESTURE)) {
             mFodGestureEnable = TunerService.parseIntegerSwitch(newValue, false);
-        } else {
+        } else if (key.equals(Settings.Secure.DOZE_ENABLED)) {
             mDozeEnabled = TunerService.parseIntegerSwitch(newValue, true);
         }
     }
