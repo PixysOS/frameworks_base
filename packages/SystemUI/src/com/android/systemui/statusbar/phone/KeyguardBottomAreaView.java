9d7391abaf2fbf9e3669fa1621a43bd490ea9371
AshutoshSundresh <ashutoshsundresh@gmail.com>
2021-07-24 17:13:07 +0530

    overlays: Add an OxygenOS icon pack
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Change-Id: Iaaecb20abab3f387b17f9410aae9b573da4791ad

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -931,7 +931,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
             if (mLeftIsVoiceAssist) {
                 mIconState.isVisible = mUserSetupComplete && mShowLeftAffordance;
                 if (mLeftAssistIcon == null) {
-                    mIconState.drawable = mContext.getDrawable(R.drawable.ic_mic_26dp);
+                    mIconState.drawable = mContext.getDrawable(R.drawable.ic_mic_ssos);
                 } else {
                     mIconState.drawable = mLeftAssistIcon;
                 }
@@ -940,8 +940,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
             } else {
                 mIconState.isVisible = mUserSetupComplete && mShowLeftAffordance
                         && isPhoneVisible();
-                mIconState.drawable = mContext.getDrawable(
-                        com.android.internal.R.drawable.ic_phone);
+                mIconState.drawable = mContext.getDrawable(R.drawable.ic_phone_ssos);
                 mIconState.contentDescription = mContext.getString(
                         R.string.accessibility_phone_button);
             }
@@ -965,7 +964,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
                     && mShowCameraAffordance
                     && mUserSetupComplete
                     && resolveCameraIntent() != null;
-            mIconState.drawable = mContext.getDrawable(R.drawable.ic_camera_alt_24dp);
+            mIconState.drawable = mContext.getDrawable(R.drawable.ic_camera_ssos);
             mIconState.contentDescription =
                     mContext.getString(R.string.accessibility_camera_button);
             return mIconState;
