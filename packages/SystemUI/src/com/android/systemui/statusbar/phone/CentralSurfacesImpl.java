daddbae81511ffdc600dce18c3bac46edd9fbbd7
ezio84 <brabus84@gmail.com>
2021-10-21 18:07:40 +0000

    base: Long press power button to toggle flashlight [1/2]
     * Thanks to beanstown106 for the initial longpress action calls in
       PhoneWindowManager (improved by lineage guys)
    
     * Remove the settings pref if the device doesn't have flashlight
    
     * Vibrate when toggling torch to let the user know of unwanted toggling
       in the pocket (even if vibration is disabled by user)
    
     * Allow torch action also on ambient display
    
    [ezio84] :
    - Fix after Android 12L merge
    
    [AEX Edits] :
    - Port to S
    - simplify code
    
    [mydongistiny]:
    - Fixed for android10
    
    [DennySPB] :
    - base: ScreenOff torch: Allow to disable torch when screen is on
    
    [mickaelmendes50] :
    - Adapt to AOSPA
    - Adapt to Android 13
    
     * We don't allow users to use the Double Press power option,
       so remove the non used code.
     * Removed "double press power to toggle torch"
     * Removed "handle camera gesture here instead of extensions"
     * Removed "Add 170ms delay for double tap action"
    
    Change-Id: I12da044f86c7b625872607529cf8524615cf576b
    Co-authored-by: DennySPb <dennyspb@gmail.com>
    Co-authored-by: Mesquita <mesquita@aospa.co>
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -362,6 +362,11 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
         return mQSPanelController;
     }
 
+    /** */
+    public void toggleCameraFlash() {
+        mCommandQueueCallbacks.toggleCameraFlash();
+    }
+
     /**
      * The {@link StatusBarState} of the status bar.
      */
--- packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/CentralSurfacesCommandQueueCallbacksTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/CentralSurfacesCommandQueueCallbacksTest.java
@@ -53,6 +53,7 @@ import com.android.systemui.statusbar.disableflags.DisableFlagsLogger;
 import com.android.systemui.statusbar.notification.stack.NotificationStackScrollLayoutController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.statusbar.policy.HeadsUpManager;
+import com.android.systemui.statusbar.policy.FlashlightController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.policy.RemoteInputQuickSettingsDisabler;
 
@@ -95,6 +96,7 @@ public class CentralSurfacesCommandQueueCallbacksTest extends SysuiTestCase {
     @Mock private UserTracker mUserTracker;
     @Mock private QSHost mQSHost;
     @Mock private ActivityStarter mActivityStarter;
+    @Mock private FlashlightController mFlashlightController;
 
     CentralSurfacesCommandQueueCallbacks mSbcqCallbacks;
 
@@ -130,7 +132,8 @@ public class CentralSurfacesCommandQueueCallbacksTest extends SysuiTestCase {
                 mCameraLauncherLazy,
                 mUserTracker,
                 mQSHost,
-                mActivityStarter);
+                mActivityStarter,
+                mFlashlightController);
 
         when(mUserTracker.getUserHandle()).thenReturn(
                 UserHandle.of(ActivityManager.getCurrentUser()));
