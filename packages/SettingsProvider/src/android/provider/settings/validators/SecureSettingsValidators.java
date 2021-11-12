e4f654315a48723796edd520f4179c4eaed2fb4a
acras01 <acras1@gmail.com>
2021-11-12 08:14:16 +0200

    SystemUI: Allow toggling combined signal icons [1/2]
    * @jhonboy121: Use the feature flag as a default value, otherwise there is no point in having a "toggle" if the feature flag is not enabled
    
    idoybh (YAAP): Add to the backup array, Adapt to Sv2
    
    Change-Id: If869fe31e43f198c4ced8e872c5c060a524f6462
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
@@ -340,5 +340,6 @@ public class SecureSettingsValidators {
             return true;
         });
         VALIDATORS.put(Secure.FACE_UNLOCK_METHOD, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(Secure.SHOW_COMBINED_STATUS_BAR_SIGNAL_ICONS, BOOLEAN_VALIDATOR);
     }
 }
