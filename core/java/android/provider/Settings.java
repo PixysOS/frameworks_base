dffc5e9ed2de46dab8b454e8560009fe03a33c3f
Dil3mm4 <dil3mm4.dev@gmail.com>
2021-12-31 11:21:49 +0800

    base: Implement background process killer [1/2]
    * rebranded/adapted from Descendant-XI/frameworks_base@4d6a3fa
    * nuked stuff we dont want to mess with like sync and location
    
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6327,6 +6327,13 @@ public final class Settings {
         public static final String POCKET_JUDGE = "pocket_judge";
 
         /**
+         * Arcane Idle Manager
+         * @hide
+         */
+        @Readable
+        public static final String ARCANE_IDLE_MANAGER = "arcane_idle_manager";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -110,6 +110,7 @@ import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.keyguard.ViewMediatorCallback;
 import com.android.systemui.ActivityIntentHelper;
+import com.android.systemui.ArcaneIdleManager;
 import com.android.systemui.AutoReinflateContainer;
 import com.android.systemui.CoreStartable;
 import com.android.systemui.DejankUtils;
@@ -443,6 +444,9 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
     /** Controller for the Shade. */
     private final ShadeSurface mShadeSurface;
     private final ShadeLogger mShadeLogger;
+
+   // Arcane Idle
+    private boolean isIdleManagerIstantiated = false;
 
     // settings
     private QSPanelController mQSPanelController;
@@ -2502,6 +2506,17 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
             }
 
             DejankUtils.stopDetectingBlockingIpcs(tag);
+            if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                                              Settings.System.ARCANE_IDLE_MANAGER, 1,
+                                              mLockscreenUserManager.getCurrentUserId()) == 1) {
+                if (!isIdleManagerIstantiated) {
+                    ArcaneIdleManager.initManager(mContext);
+                    isIdleManagerIstantiated = true;
+                    ArcaneIdleManager.executeManager();
+                } else {
+                    ArcaneIdleManager.executeManager();
+                }
+            }
         }
 
         @Override
@@ -2568,6 +2583,11 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
                 }
             });
             DejankUtils.stopDetectingBlockingIpcs(tag);
+            if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                                              Settings.System.ARCANE_IDLE_MANAGER, 1,
+                                              mLockscreenUserManager.getCurrentUserId()) == 1) {
+                ArcaneIdleManager.haltManager();
+            }
         }
 
         /**
