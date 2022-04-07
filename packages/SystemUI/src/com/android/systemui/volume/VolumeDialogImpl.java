d5bf73e7174647fde0e8c2f2ddaf1d103cc3de3d
jhenrique09 <jhsv09@gmail.com>
2022-04-07 18:24:11 +0000

    VolumeDialogImpl: Fix cuttered layout when on setup or lock task mode
    Change-Id: I20aacbda4e1a90cad3120ed6f4a65c8c884521cf

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -312,6 +312,8 @@ public class VolumeDialogImpl implements VolumeDialog,
         }
     }
 
+    private FrameLayout mRoundedBorderBottom;
+
     public VolumeDialogImpl(
             Context context,
             VolumeDialogController volumeDialogController,
@@ -624,6 +626,8 @@ public class VolumeDialogImpl implements VolumeDialog,
 
         mSettingsView = mDialog.findViewById(R.id.settings_container);
         mSettingsIcon = mDialog.findViewById(R.id.settings);
+
+        mRoundedBorderBottom = mDialog.findViewById(R.id.rounded_border_bottom);
 
         if (mVolumePanelOnLeft) {
             if (mRingerAndDrawerContainer != null) {
@@ -1161,6 +1165,11 @@ public class VolumeDialogImpl implements VolumeDialog,
     }
 
     private void initSettingsH(int lockTaskModeState) {
+        if (mRoundedBorderBottom != null){
+            mRoundedBorderBottom.setVisibility(!mDeviceProvisionedController.isCurrentUserSetup() ||
+                    lockTaskModeState != LOCK_TASK_MODE_NONE
+                    ? VISIBLE : GONE);
+        }
         if (mSettingsView != null) {
             mSettingsView.setVisibility(
                     mDeviceProvisionedController.isCurrentUserSetup() &&
