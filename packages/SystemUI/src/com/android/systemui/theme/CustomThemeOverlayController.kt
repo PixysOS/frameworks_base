4d98720d59c660bdbc0ecc6a4cdd6d1aaeebd808
Anay Wadhera <anay1018@gmail.com>
2022-10-12 13:18:49 +0800

    SystemUI: use color seed generation algorithm from AOSP
    The primary wallpaper color is not always the most vibrant or expected accent color, so change this behavior to get the best average color by using the AOSP monet algorithm
    
    Change-Id: I111849baea49919131565d72587ab0d91e5efc2d | AOSP
    Reviewed-on: https://review.statixos.com/c/android_packages_apps_Statix_SystemUI/+/7586
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- packages/SystemUI/src/com/android/systemui/theme/CustomThemeOverlayController.kt
+++ packages/SystemUI/src/com/android/systemui/theme/CustomThemeOverlayController.kt
@@ -43,6 +43,7 @@ import com.android.systemui.dagger.qualifiers.Main
 import com.android.systemui.dump.DumpManager
 import com.android.systemui.flags.FeatureFlags
 import com.android.systemui.keyguard.WakefulnessLifecycle
+import com.android.systemui.monet.ColorScheme
 import com.android.systemui.monet.Style
 import com.android.systemui.settings.UserTracker
 import com.android.systemui.statusbar.policy.ConfigurationController
@@ -212,7 +213,7 @@ class CustomThemeOverlayController @Inject constructor(
 
     // Seed colors
     override fun getNeutralColor(colors: WallpaperColors) = colors.primaryColor.toArgb()
-    override fun getAccentColor(colors: WallpaperColors) = getNeutralColor(colors)
+    override fun getAccentColor(colors: WallpaperColors) = ColorScheme.getSeedColor(colors)
 
     override protected fun getOverlay(color: Int, type: Int, style: Style): FabricatedOverlay {
         // Generate color scheme
