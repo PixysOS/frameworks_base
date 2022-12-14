49133d8758ea5fc6ff72fc0497aa4afa29cead90
Pranav Vashi <neobuddy89@gmail.com>
2022-12-14 08:22:21 +0530

    Slightly optimize less boring apps check for heads up
    * We do not want to keep checking packages everytime
      when this option is disabled or device is dozing.
    
    Change-Id: I3e2f3803923727ef4a5a04b9bfd6a578809d03fe
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/interruption/NotificationInterruptStateProviderImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/interruption/NotificationInterruptStateProviderImpl.java
@@ -398,7 +398,7 @@ public class NotificationInterruptStateProviderImpl implements NotificationInter
             return false;
         }
 
-        if (shouldSkipHeadsUp(sbn)) {
+        if (mLessBoringHeadsUp && shouldSkipHeadsUp(sbn)) {
             // mLogger.logNoHeadsUpShouldSkipPackage(sbn);
             return false;
         }
@@ -504,13 +504,14 @@ public class NotificationInterruptStateProviderImpl implements NotificationInter
     }
 
     public boolean shouldSkipHeadsUp(StatusBarNotification sbn) {
+        if (mStatusBarStateController.isDozing()) return false;
         String notificationPackageName = sbn.getPackageName();
 
         boolean isLessBoring = notificationPackageName.equals(getDefaultDialerPackage(mTm))
                 || notificationPackageName.equals(getDefaultSmsPackage(mContext))
                 || notificationPackageName.toLowerCase().contains("clock");
 
-        return !mStatusBarStateController.isDozing() && mLessBoringHeadsUp && !isLessBoring;
+        return !isLessBoring;
     }
 
     private static String getDefaultSmsPackage(Context ctx) {
