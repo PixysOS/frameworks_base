ae9f11b3778005526aafd37b19683350d834c0fa
Ido Ben-Hur <idoybh2@gmail.com>
2023-01-09 03:38:02 +0200

    base: Pocket Judge: Don't go to sleep while on call
    This causes gdialer to stop vibrations. Let's not.
    
    Also:
    * Properly resume listening on fingerprints
    * Use TYPE_SECURE_SYSTEM_OVERLAY for the pocket lock view to actually overlay above everything
      (see com.android.server.policy.WindowManagerPolicy for a full list of priorities)
    
    This commit also reverts previous temp solutions to this issue:
    
    Revert "base: Allow enabling pocket judge on call via an overlay"
    
    This reverts commit 52a12f033992a454b9af6f7ea1c1ea5e2a9f9b07.
    
    Revert "base: Pocket Judge: Don't block dialer UI"
    
    This reverts commit e9dff2d370237ff98bdb7ee07f58239869098807.
    
    Change-Id: I09b962f96189aad2447505b2d7b6b7c510a20ec7

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -2562,7 +2562,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
                         notifyAboutEnrollmentChange(msg.arg1);
 			break;
                     case MSG_POCKET_STATE_CHANGED:
-                        updateBiometricListeningState(BIOMETRIC_ACTION_UPDATE);
+                        updateFingerprintListeningState(BIOMETRIC_ACTION_UPDATE);
                         break;
                     default:
                         super.handleMessage(msg);
