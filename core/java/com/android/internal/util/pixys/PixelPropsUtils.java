51214347770d3a5fa40764af9a9c8e738779a4d7
aswin7469 <aswinas@pixysos.com>
2023-01-15 16:11:33 +0530

    PixelPropsUtils: Rework the spoofs
    Change-Id: I6a0f9b43ff15cc7ef0f083cb92cfd68962fb24b5
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/java/com/android/internal/util/pixys/PixelPropsUtils.java
+++ core/java/com/android/internal/util/pixys/PixelPropsUtils.java
@@ -46,18 +46,11 @@ public class PixelPropsUtils {
     private static final Map<String, Object> propsToChangeMI11;
     private static final Map<String, ArrayList<String>> propsToKeep;
 
-    private static final String[] packagesToChangePixel7Pro = {
+    private static final String[] packagesToChangePixel5 = {
             "com.google.android.gms",
-            "com.google.android.apps.privacy.wildlife",
-            "com.google.android.apps.wallpaper.pixel",
-            "com.google.android.apps.wallpaper",
-            "com.google.android.apps.subscriptions.red",
             "com.google.android.inputmethod.latin",
-            "com.google.android.apps.wellbeing",
-            "com.google.android.as",
-            "com.google.android.gms.persistent",
-            "com.google.android.googlequicksearchbox"
-
+            "com.google.android.googlequicksearchbox",
+            "com.google.android.tts"
     };
 
     private static final String[] packagesToChangePixelXL = {
@@ -205,6 +198,7 @@ public class PixelPropsUtils {
         propsToChangeMI11.put("DEVICE", "star");
         propsToChangeMI11.put("PRODUCT", "star");
         propsToChangeMI11.put("MODEL", "M2102K1G");
+
     }
 
     public static void setProps(String packageName) {
@@ -224,7 +218,7 @@ public class PixelPropsUtils {
                     propsToChange.putAll(propsToChangePixelXL);
                 } else {
                     if (isPixelDevice) return;
-                    propsToChange.putAll(propsToChangePixel5);
+                    propsToChange.putAll(propsToChangePixel7Pro);
                 }
             } else if (packageName.equals("com.netflix.mediaclient") && 
                         !SystemProperties.getBoolean("persist.sys.pixelprops.netflix", false)) {
@@ -236,12 +230,12 @@ public class PixelPropsUtils {
                 sIsFinsky = true;
                 return;
             } else {
-                if (Arrays.asList(packagesToChangePixel7Pro).contains(packageName)) {
-                    propsToChange.putAll(propsToChangePixel7Pro);
+                if (Arrays.asList(packagesToChangePixel5).contains(packageName)) {
+                    propsToChange.putAll(propsToChangePixel5);
                 } else if (Arrays.asList(packagesToChangePixelXL).contains(packageName)) {
                     propsToChange.putAll(propsToChangePixelXL);
                 } else {
-                    propsToChange.putAll(propsToChangePixel5);
+                    propsToChange.putAll(propsToChangePixel7Pro);
                 }
             }
 
