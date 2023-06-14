a4a6514cda1dffca8ebe6685257242147454c4db
Michael Bestas <mkbestas@gmail.com>
2023-06-14 11:53:23 +0300

    SystemUI: Restore wifi and cellular QS tiles
    Change-Id: Ibbbabf30be08c6852f6aa968d28de94ac29b0e71

--- packages/SystemUI/src/com/android/systemui/qs/dagger/pixys/QSModulePixys.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/pixys/QSModulePixys.kt
@@ -17,9 +17,11 @@
 package com.android.systemui.qs.dagger.pixys
 
 import com.android.systemui.qs.tileimpl.QSTileImpl
+import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.AODTile
+import com.android.systemui.qs.tiles.WifiTile
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -28,6 +30,12 @@ import dagger.multibindings.StringKey
 @Module
 interface QSModulePixys {
 
+    /** Inject CellularTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CellularTile.TILE_SPEC)
+    fun bindCellularTile(cellularTile: CellularTile): QSTileImpl<*>
+    
     /** Inject PowerShareTile into tileMap in QSModule */
     @Binds
     @IntoMap
@@ -45,4 +53,10 @@ interface QSModulePixys {
     @IntoMap
     @StringKey(AODTile.TILE_SPEC)
     fun bindAODTile(aodTile: AODTile): QSTileImpl<*>
+
+    /** Inject WifiTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(WifiTile.TILE_SPEC)
+    fun bindWifiTile(wifiTile: WifiTile): QSTileImpl<*>
 }
