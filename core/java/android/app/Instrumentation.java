e72f78e73bf4be34ef4572bc6a784f79d91eccaa
someone5678 <someone5678@users.noreply.github.com>
2024-02-12 05:46:38 +0900

    PixelPropUtils: Refactor again
    squashed -->
    
    *  Move play integrity spoofing to props
    *  Fix sign-in issues on some apps
    *  Dont spoof gcam process as it will break older pixels
    
    based on -->
    
    * https://github.com/minaripenguin/android_frameworks_base/commit/d2e21df4df5dc22d2288f9462661bf742ac8f155
    * https://github.com/TheParasiteProject/frameworks_base/commit/8ec09ed48edceb942ce91e6d3a4518d4563ebdeb
    * https://github.com/minaripenguin/android_frameworks_base/commit/48b505efc2664a42d82080bfced61f143eed3cdd
    
    Co-authored-by: aswin7469 <aswinas@pixysos.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -1353,7 +1353,7 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        PixelPropsUtils.setProps(app);
+        PixelPropsUtils.setProps(context);
         GamesPropsUtils.setProps(app);
         return app;
     }
@@ -1372,7 +1372,7 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        PixelPropsUtils.setProps(app);
+        PixelPropsUtils.setProps(context);
         GamesPropsUtils.setProps(app);
         return app;
     }
