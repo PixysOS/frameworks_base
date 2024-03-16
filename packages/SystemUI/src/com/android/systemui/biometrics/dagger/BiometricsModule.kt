ad24bc310ce7fe7f62c910ffaa68b1cbfd845c94
Anay Wadhera <anay1018@gmail.com>
2024-03-16 15:41:55 -0400

    fixup! SystemUI: Add FingerprintInteractiveToAuthProvider implementation
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: Ie21f0acc8ae670dcb81486d8f75b640197d40c25

--- packages/SystemUI/src/com/android/systemui/biometrics/dagger/BiometricsModule.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/dagger/BiometricsModule.kt
@@ -22,6 +22,8 @@ import com.android.internal.R
 import com.android.systemui.CoreStartable
 import com.android.systemui.biometrics.AuthController
 import com.android.systemui.biometrics.EllipseOverlapDetectorParams
+import com.android.systemui.biometrics.FingerprintInteractiveToAuthProvider
+import com.android.systemui.biometrics.FingerprintInteractiveToAuthProviderImpl
 import com.android.systemui.biometrics.UdfpsUtils
 import com.android.systemui.biometrics.data.repository.BiometricStatusRepository
 import com.android.systemui.biometrics.data.repository.BiometricStatusRepositoryImpl
@@ -39,11 +41,12 @@ import com.android.systemui.biometrics.udfps.BoundingBoxOverlapDetector
 import com.android.systemui.biometrics.udfps.EllipseOverlapDetector
 import com.android.systemui.biometrics.udfps.OverlapDetector
 import com.android.systemui.biometrics.ui.binder.SideFpsOverlayViewBinder
-import com.android.systemui.biometrics.FingerprintInteractiveToAuthProvider
-import com.android.systemui.biometrics.FingerprintInteractiveToAuthProviderImpl
+import com.android.systemui.dagger.qualifiers.Background
 import com.android.systemui.dagger.SysUISingleton
 import com.android.systemui.statusbar.policy.ConfigurationController.ConfigurationListener
+import com.android.systemui.user.domain.interactor.SelectedUserInteractor
 import com.android.systemui.util.concurrency.ThreadFactory
+import com.android.systemui.util.settings.SecureSettings
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -52,6 +55,7 @@ import dagger.multibindings.IntoMap
 import dagger.multibindings.IntoSet
 import java.util.concurrent.Executor
 import javax.inject.Qualifier
+import kotlinx.coroutines.CoroutineDispatcher
 
 /** Dagger module for all things biometric. */
 @Module
@@ -106,8 +110,18 @@ interface BiometricsModule {
             threadFactory.buildExecutorOnNewThread("biometrics")
 
         @Provides
-        fun providesFingerprintInteractiveToAuth(ctx: Context): FingerprintInteractiveToAuthProvider =
-            FingerprintInteractiveToAuthProviderImpl(ctx);
+        fun providesFingerprintInteractiveToAuth(
+            @Background backgroundDispatcher: CoroutineDispatcher,
+            context: Context,
+            secureSettings: SecureSettings,
+            selectedUserInteractor: SelectedUserInteractor,
+        ): FingerprintInteractiveToAuthProvider =
+            FingerprintInteractiveToAuthProviderImpl(
+                backgroundDispatcher,
+                context,
+                secureSettings,
+                selectedUserInteractor,
+            )
 
         @Provides fun providesUdfpsUtils(): UdfpsUtils = UdfpsUtils()
 
