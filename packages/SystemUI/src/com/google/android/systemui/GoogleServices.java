dcd8d39107309a4bf62fcc4dfe431bc761ffbb9b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2024-03-23 01:13:03 +0530

    SystemUI: Implement ambient music support
    [aswin7469] : Rebranded, fixed for 14 QPR2 and moved directly to fw base
    
    Co-authored-by: Anay Wadhera <anay1018@gmail.com>
    Change-Id: If97543ef26aa1e747b637ffc9b128eaa1c64ac53
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

---
+++ packages/SystemUI/src/com/google/android/systemui/GoogleServices.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2022 StatiXOS
+ * SPDX-License-Identifer: Apache-2.0
+ */
+
+package com.google.android.systemui;
+
+import android.app.AlarmManager;
+import android.content.Context;
+
+import com.android.systemui.Dumpable;
+import com.android.systemui.VendorServices;
+import com.android.systemui.dagger.SysUISingleton;
+import com.android.systemui.plugins.ActivityStarter;
+import com.android.systemui.power.domain.interactor.PowerInteractor;
+import com.android.systemui.shade.NotificationShadeWindowView;
+import com.android.systemui.shade.ShadeViewController;
+import com.android.systemui.util.wakelock.WakeLockLogger;
+
+import com.google.android.systemui.ambient.AmbientIndicationContainer;
+import com.google.android.systemui.ambient.AmbientIndicationService;
+import com.android.systemui.res.R;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+
+@SysUISingleton
+public class GoogleServices extends VendorServices {
+
+    private final ArrayList<Object> mServices = new ArrayList<>();
+    private final ActivityStarter mActivityStarter;
+    private final AlarmManager mAlarmManager;
+    private final NotificationShadeWindowView mNotificationShadeWindowView;
+    private final PowerInteractor mPowerInteractor;
+    private final ShadeViewController mShadeViewController;
+    private final WakeLockLogger mWakelockLogger;
+
+    private final Context mContext;
+
+    @Inject
+    public GoogleServices(
+            Context context,
+            ActivityStarter activityStarter,
+            AlarmManager alarmManager,
+            NotificationShadeWindowView notificationShadeWindowView,
+            PowerInteractor powerInteractor,
+            ShadeViewController shadeViewController,
+            WakeLockLogger wakeLockLogger) {
+        super();
+        mActivityStarter = activityStarter;
+        mAlarmManager = alarmManager;
+        mContext = context;
+        mNotificationShadeWindowView = notificationShadeWindowView;
+        mPowerInteractor = powerInteractor;
+        mShadeViewController = shadeViewController;
+        mWakelockLogger = wakeLockLogger;
+    }
+
+    @Override
+    public void start() {
+        AmbientIndicationContainer ambientIndicationContainer =
+                (AmbientIndicationContainer)
+                        mNotificationShadeWindowView.findViewById(
+                                R.id.ambient_indication_container);
+        ambientIndicationContainer.initializeView(
+                mShadeViewController, mPowerInteractor, mActivityStarter, mWakelockLogger);
+        addService(
+                new AmbientIndicationService(mContext, ambientIndicationContainer, mAlarmManager));
+    }
+
+    @Override
+    public void dump(PrintWriter printWriter, String[] strArr) {
+        for (int i = 0; i < mServices.size(); i++) {
+            if (mServices.get(i) instanceof Dumpable) {
+                ((Dumpable) mServices.get(i)).dump(printWriter, strArr);
+            }
+        }
+    }
+
+    private void addService(Object obj) {
+        if (obj != null) {
+            mServices.add(obj);
+        }
+    }
+}
