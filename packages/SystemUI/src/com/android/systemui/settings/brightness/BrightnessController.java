f422c95ca6d48b94915341c7029c12c11d107f32
aswin7469 <aswinas@pixysos.com>
2024-06-23 17:52:49 +0530

    Revert "SystemUI: Add haptics to brightness slider"
    This reverts commit 621608e3110b7ac0d100c7048f1ef0638856b57f.

--- packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
@@ -36,9 +36,6 @@ import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.UserHandle;
 import android.os.UserManager;
-import android.os.Vibrator;
-import android.os.VibratorManager;
-import android.os.VibrationEffect;
 import android.provider.Settings;
 import android.service.vr.IVrManager;
 import android.service.vr.IVrStateCallbacks;
@@ -114,14 +111,6 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
     private float mBrightnessMax = PowerManager.BRIGHTNESS_MAX;
 
     private ValueAnimator mSliderAnimator;
-
-    private final boolean mHasVibrator;
-    private final Vibrator mVibrator;
-    private final VibratorManager mVibratorManager;
-    private static final VibrationEffect BRIGHTNESS_SLIDER_HAPTIC =
-            VibrationEffect.get(VibrationEffect.EFFECT_TEXTURE_TICK);
-
-    private static int mLastTrackingUpdate = 0;
 
     @Override
     public void setMirror(BrightnessMirrorController controller) {
@@ -339,10 +328,6 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
         mAutomaticAvailable = context.getResources().getBoolean(
                 com.android.internal.R.bool.config_automatic_brightness_available);
 
-        mVibratorManager = (VibratorManager) mContext.getSystemService(Context.VIBRATOR_MANAGER_SERVICE);
-        mVibrator = mVibratorManager.getDefaultVibrator();
-        mHasVibrator = mVibrator != null && mVibrator.hasVibrator();
-
         if (mIcon != null) {
             if (mAutomaticAvailable) {
                 mIcon.setOnClickListener(new View.OnClickListener() {
@@ -405,13 +390,6 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
 
         }
         setBrightness(valFloat);
-
-        mLastTrackingUpdate = (mLastTrackingUpdate + 1) % 5;
-
-        // Give haptic feedback every 5 changes, only if brightness is changed manually
-        if (mHasVibrator && tracking && mLastTrackingUpdate == 0)
-            mVibrator.vibrate(BRIGHTNESS_SLIDER_HAPTIC);
-
         if (!tracking) {
             AsyncTask.execute(new Runnable() {
                     public void run() {
